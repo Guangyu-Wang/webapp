deee64d03c3710d515a3520e0153721887b97fca
Guangyu Wang <wang.guangyu1@northeastern.edu>
2023-02-08 10:36:03 -0500

    add production routes

--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -1,11 +1,20 @@
+const product = require("../models/product.js");
+
 module.exports = app => {
     const tutorials = require("../controllers/tutorial.controller.js");
+    const production = require("../controllers/product.controller");
     var router = require("express").Router();
 
     router.post("/user", tutorials.create);
     router.put("/user/update", tutorials.update);
     router.get("/user/find", tutorials.findUser);
     router.get("/user/findid", tutorials.findUserById);
+
+    router.post("/production", production.createProduction);
+    router.get("/production/getbyid", production.findById);
+    router.put("/production/update", production.update);
+    router.patch("/production/update", production.update);
+    router.delete("/production/delete", production.delete);
 
     app.use('/api/', router);
 }
