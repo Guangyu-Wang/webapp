2e49d4c5143a0d2eb6bd21c63e8c5587b0012075
Guangyu Wang <97964727+GuangyuWang-NEU@users.noreply.github.com>
2023-03-02 21:28:02 -0500

    Assign4 (#44)
    * Update index.js
    
    * Update index.js
    
    * Update package.json
    
    * Update package.json
    
    * install
    
    * packer
    
    * new
    
    * new
    
    * change
    
    * service
    
    * zip
    
    * image
    
    * image1
    
    * delete zip
    
    * change
    
    * 1
    
    * new
    
    * image controller;
    
    * new
    
    * delete
    
    * change
    
    * change
    
    * ichange
    
    * new
    
    * 1
    
    * hi
    
    * 1
    
    * 1
    
    * q
    
    * q
    
    * hi
    
    * 1
    
    * 2
    
    * 1
    
    * e
    
    * w
    
    * 2
    
    * e
    
    * rt
    
    * hello
    
    * qw
    
    * kk
    
    * Update my-template.json
    
    * ee
    
    * w
    
    * sd
    
    * 1
    
    * 1
    
    * q
    
    * image
    
    * hello
    
    * new
    
    * rds
    
    * new
    
    * new
    
    * new

--- app/models/index.js
+++ app/models/index.js
@@ -1,4 +1,4 @@
-const dbConfig = require("../config/db.config.js");
+const dbConfig = require("../config/db.config-ec2.js");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
