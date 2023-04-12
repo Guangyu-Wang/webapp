5e56977adbcb8b845e2b87cab151ffd3e0675af7
Guangyu Wang <wang.guangyu1@northeastern.edu>
2023-04-12 17:42:40 -0400

    heakth

--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -24,5 +24,7 @@ module.exports = app => {
     router.get("/product/:product_id/image/:image_id", image.findById);
     router.delete("/product/:product_id/image/:image_id", image.delete);
 
+    router.get("/health", tutorials.health);
+
     app.use('/v1/', router);
 }
